3b74bdbdb2e2fbb1682551f6ed6522b852418076
Tyler Boright <tyler.boright@gmail.com>
2020-06-23 17:19:01 +0800

    adds a basic package.json, lock, node script, and default postman collection to the repo

---
+++ main.js
@@ -0,0 +1,19 @@
+const nm = require('newman');
+
+nm.run({
+    collection: require('./meepshop-product-api-automation.postman_collection.json'),
+    reporters: 'cli'
+})
+    .on('start', (err, args) => {
+        console.log('running meepshop collection')
+    })
+    .on('done', (err, summary) => {
+        if (err || summary.error) {
+            console.error('collection threw the following error while executing: ', err === undefined ? summary.err : err)
+        } 
+        console.log(summary.run.failures);
+        console.log(summary.run.stats);
+        //if (summary.run.failures.length > 0) {
+             
+        //}
+    });
